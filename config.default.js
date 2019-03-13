602f51c3e47f58693973ba89c80ddb1c8b8d22da
dodying <549779963@qq.com>
2019-03-13 08:43:20 +0800

    update

--- config.default.js
+++ config.default.js
@@ -4,8 +4,11 @@ let config = {
   debug: false,
   locale: 'zh-CN', // 决定时间格式
   ignoreWarn: { // 无视警告
-    mode3: true,
-    preferPath: true
+    mode1: false,
+    mode3: false,
+    preferPath: false,
+    install: false,
+    withoutVirusScan: false
   },
   profile: {
     example: {
@@ -16,6 +19,13 @@ let config = {
   commercialMode: 0, // 对于商业软件 0:只检查有无新版本 1.下载新版本安装包后不自动安装 2.下载并自动安装新版本 3.删除原版本（包括配置），并安装新版本
   freePersion: true, // 是否将 Free Personal 类型的软件是为商业软件
   freemium: true, // 是否将 Freemium 类型的软件是为商业软件
+  virus: { // 检查病毒
+    apiKey: '', // 获取地址: https://www.virustotal.com/#/settings/apikey (留空则不检查)
+    upload: true, // 是否上传文件以分析
+    ignoreSoftware: [], // 忽略来自某软件的检查结果
+    ignoreResult: [], // 忽略来自某重结果
+    safeDetectionRatio: 0 // 视为安全的检出率
+  },
   specialMode: { // 为特定软件设置特定模式(最优先)
   },
   useProxy: 1, // 是否使用代理(包括请求与下载) 0.不使用 1.如果配置中声明，则使用 2.强制使用
@@ -27,22 +37,19 @@ let config = {
   ],
   urlWithoutProxy: [ // 请求与下载的链接如果匹配任一，则不走代理(最优先)
   ],
-  request: {// 请求相关
+  request: {
+    retry: 5, // 重试次数(0表示不重试)
     timeout: 60, // 请求超时(单位秒)
-    proxyHTTP: 'http://user:password@host:port', // HTTP代理
-    proxySocks: true, // Socks代理(有则优先使用Socks代理)
-    proxySocksHost: 'host', // Socks代理-域名，留空则为localhost
-    proxySocksPort: 2345, // Socks代理-端口
-    proxySocksUsername: 'user', // Socks代理-验证(无则留空)
-    proxySocksPassword: 'password' // Socks代理-验证(无则留空)
+    proxy: 'protocol://username:password@hostname:port' // 代理，留空则不使用代理 protocol: socks5/http
   },
   download: { // 下载相关
-    method: 'aria2c', // aria2c(支持多进程) 或 wget 或 curl(支持socks代理)
+    method: 'request', // request(无需下载，但不支持续传) 或 aria2c(支持多进程) 或 wget 或 curl(支持socks代理)
     quiet: false, // 不显示进度
     continue: true, // 是否断点续传
     retry: 5, // 重试次数(0表示无数次)
+    retryDelay: 5, // 重试延迟(单位秒)
     timeout: 600, // 超时(单位秒，0表示无超时)
-    proxy: '[protocol://]host[:port]', // 代理，留空则不使用代理
+    proxy: 'protocol://username:password@hostname:port', // 代理，留空则不使用代理
     urlWithoutHeader: [ // 下载链接如果匹配任一，则下载时不包括Header，Header包括userAgent/referer
     ],
     sfMirror: 'nchc'// sf下载镜像
@@ -50,6 +57,9 @@ let config = {
   preserveArchive: true, // 安装后是否保留安装包
   preserveOldArchive: false, // 当有新版本时，是否保留旧版本
   saveVersion: true, // 是否保存版本信息(推荐true)
+  includeGlobal: [ // 安装中要保留的文件/文件夹(优先于excludeGlobal)
+    'locales', 'resources'
+  ],
   excludeGlobal: [ // 安装中要排除的文件/文件夹
     /^uninstall.exe/i // 相对于软件根路径
   ],
