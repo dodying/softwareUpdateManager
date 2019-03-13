39389951d4cfcef6f7c00831a705a1f848dda261
dodying <549779963@qq.com>
2019-03-13 13:06:23 +0800

    移动software到[software-for-softwareUpdateManager)
    [software-for-softwareUpdateManager)(https://github.com/dodying/software-for-softwareUpdateManager)

--- software/Universal Extractor mod by koros aka ya158.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-let data = {
-  url: 'http://forum.oszone.net/thread-295084.html',
-  version: {
-    func: async (res, $) => $('.fieldset>div>b:nth-child(3)').eq(0).text().split('-').reverse().join('-')
-  }
-}
-module.exports = data
--- software/冰点文库下载器.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-let data = {
-  url: 'http://www.bingdian001.com/?p=30',
-  version: {
-    selector: 'h2'
-  }
-}
-module.exports = data
